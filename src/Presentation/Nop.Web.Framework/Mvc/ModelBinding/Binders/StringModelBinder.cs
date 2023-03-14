6e5c54221355d924ebb2ae5fc276b19a117975d8
Sergey Koshelev <sergey.k@nopcommerce.com>
2023-03-14 09:18:18 +0300

    Added trimming all entered string values

---
+++ src/Presentation/Nop.Web.Framework/Mvc/ModelBinding/Binders/StringModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Nop.Web.Framework.Mvc.ModelBinding.Binders
+{
+    /// <summary>
+    /// Represents model binder for string properties
+    /// </summary>
+    public class StringModelBinder : IModelBinder
+    {
+        Task IModelBinder.BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var valueProviderResult =
+                bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (valueProviderResult.FirstValue is { } str &&
+                !string.IsNullOrEmpty(str))
+                bindingContext.Result = ModelBindingResult.Success(str.Trim());
+
+            return Task.CompletedTask;
+        }
+    }
+}
